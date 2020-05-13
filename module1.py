d3a63219f7594124512cb0e4b036f9f2b629ddd7
divyamSoni <divyam.8116@gmail.com>
2020-05-13 21:49:30 +0530

    initialized Fetch class

--- module1.py
+++ module1.py
@@ -1,2 +1,16 @@
 from can import Message
-from can.interfaces.ixxat import IXXATBus
+from can.interfaces.ixxat import IXXATBus, exceptions
+
+
+class Fetch:
+    rate = 500000
+    try:
+        bus = IXXATBus(channel=0, can_filters=[{"can_id": 0x000, "can_mask": 0x000}], bitrate=rate)
+    except exceptions.VCIDeviceNotFoundError:
+        bus = None
+    count = 0
+
+    def __init__(self):
+        self.bus = Fetch.bus
+        self.message = Message(is_extended_id=False, arbitration_id=0xfff, dlc=8,
+                               data=[0, 0, 0, 0, 0, 0, 0, 0])
